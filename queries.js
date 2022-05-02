5ce825264311fda08c91d2a470eb60c804cc045f
pessolatohenrique <pessolatohenrique@gmail.com>
2022-05-02 20:09:47 -0300

    crud de total de alunos

--- queries.js
+++ queries.js
@@ -4,3 +4,16 @@
  * Por exemplo: resgatar o total de alunos de uma plataforma de e-learning; placar eletrônico de um jogo;
  * rankings em tempo real. Entre outros exemplos
  */
+ECHO "Hello World";
+
+SET "alunos_concluidos" 120190190
+SET "perguntas_respondidas" 120190190019
+SET "perguntas_respondidas" 120190190020 // atualização
+
+SET "ultimo_aluno" "pessolatohenrique"
+
+GET "ultimo_aluno"
+GET "alunos_concluidos"
+
+DEL "ultimo_aluno"
+GET "ultimo_aluno"
